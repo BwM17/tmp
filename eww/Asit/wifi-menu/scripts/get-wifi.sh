c18290f6aeea81b20b052bb3f7d95a84cc158d56
BwM17 <Bernhard.m.w@outlook.com>
2024-09-26 21:23:44 +0200

    inital commit

---
+++ eww/Asit/wifi-menu/scripts/get-wifi.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+OUT=""
+print_networks() {
+  local WIFI=$(nmcli --fields SSID,RATE,BARS dev wifi list | sed 1d)   
+  local buff="" 
+
+  while IFS= read -r line; do  
+    read -r SSID RATE MBITS BARS <<< $line  
+    local buff+="(eventbox :class \"wifi-inner\" (box :orientation \"h\" (label :text \"$SSID $RATE $MBITS $BARS\")))"  
+  done <<< "$WIFI"   
+  
+  $OUT="(box :class \"wifi-outer\" (scroll :vscroll true $buff))"  
+}  
+
+print_networks
+eww update -c $HOME/.config/eww/wifi-menu wifi-menu=$OUT
+eww open -c $HOME/.config/eww/wifi-menu settings
+
+
+
